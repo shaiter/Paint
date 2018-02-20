ae0c498281a96ec8795cbe7eda2c23d4fce1ee7b
shaiter <Shiterartyom@gmail.com>
2018-02-20 14:12:56 +0300

    Initial Commit

---
+++ Paint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Paint
+//
+//  Created by Артём Шайтер on 2/20/18.
+//  Copyright © 2018 Артём Шайтер. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Paint/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Paint
+//
+//  Created by Артём Шайтер on 2/20/18.
+//  Copyright © 2018 Артём Шайтер. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
